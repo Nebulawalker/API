302d5eb7493f9692168cbd8b72aeccda2a609394
Roman Okatev <nebulawalker@live.ru>
2022-06-30 22:36:46 +0300

    1. Реализовано получение ссылки из аргументов скрипта 2. Обновление файла README.md 3. В репозиторий добавлен файл .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import argparse
 from urllib.parse import urlparse
 
 from dotenv import load_dotenv
@@ -50,10 +51,22 @@ def main():
     bitly_token = str(os.getenv("BITLY_TOKEN"))
 
     try:
-        link = input("Введите ссылку: ")
+        parser = argparse.ArgumentParser(
+            description="Скрипт работает с сервисом bit.ly. Обычную ссылку\
+            сокращает до вида https://bit.ly/ADCDEFG. При вводе \
+            ссылки битлинк - выводит количество переходов по ней."
+        )
+        parser.add_argument(
+            "link",
+            help="Ссылка на сайт, либо короткая ссылка bitlink"
+        )
+        link = parser.parse_args().link
 
         if is_bitlink(bitly_token, link):
-            print(count_clicks(bitly_token, link))
+            print(
+                f"Количество переходов по ссылке битли: "
+                f"{count_clicks(bitly_token, link)}"
+            )
         else:
             print(shorten_link(bitly_token, link))
 
